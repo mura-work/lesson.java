393960f81cccdc24423ea23a2326c513f1c0d8fd
murata <murata.naoya.infratop@gmail.com>
2021-05-08 12:19:38 +0900

    first commit

---
+++ src/main/java/jp/co/sss/lesson/controller/IndexController.java
@@ -0,0 +1,5 @@
+package jp.co.sss.lesson.controller;
+
+public class IndexController {
+
+}
---
+++ src/test/java/jp/co/sss/lesson/LessonApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.sss.lesson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LessonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
