afc4d96a719802c3dce67b15df74a15e80f04bc7
murata <murata.naoya.infratop@gmail.com>
2021-05-08 14:03:28 +0900

    session機能実装

--- src/main/java/jp/co/sss/lesson/controller/SessionController.java
+++ src/main/java/jp/co/sss/lesson/controller/SessionController.java
@@ -1,10 +1,10 @@
 package jp.co.sss.lesson.controller;
 
+import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-
 import jp.co.sss.lesson.form.LoginForm;
 
 @Controller
@@ -42,6 +42,27 @@ public class SessionController {
 		model.addAttribute("userId", form.getUserId());
 		model.addAttribute("password", form.getPassword());
 		return "session/loginRequest";
+	}
+	
+	@GetMapping("/loginSession")
+	public String loginSession() {
+		return "session/loginSession";
+	}
+	
+	@PostMapping("/loginSession")
+	public String loginSession(LoginForm form, HttpSession session) {
+		if (form.getUserId() ==  123) {
+			session.setAttribute("userId", form.getUserId());
+			return "redirect:/";
+		}else {
+			return "session/loginSession";
+		}
+	}
+	
+	@GetMapping("/logout")
+	public String logout(HttpSession session) {
+		session.invalidate();
+		return "redirect:/";
 	}
 	
 	
