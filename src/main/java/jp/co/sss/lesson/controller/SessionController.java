adbf4e4b41dcfe4de35e2bdca0e108b57a9df2dd
murata <murata.naoya.infratop@gmail.com>
2021-05-08 13:00:57 +0900

    postの実装まで

--- src/main/java/jp/co/sss/lesson/controller/SessionController.java
+++ src/main/java/jp/co/sss/lesson/controller/SessionController.java
@@ -2,6 +2,7 @@ package jp.co.sss.lesson.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class SessionController {
@@ -13,6 +14,12 @@ public class SessionController {
 	@GetMapping("/doLogin")
 	public String doLogin(String userId) {
 		System.out.println(userId);
+		return "session/login";
+	}
+	
+	@PostMapping("/postLogin")
+	public String postLogin(String submit) {
+		System.out.println(submit);
 		return "session/login";
 	}
 }
