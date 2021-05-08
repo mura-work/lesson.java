a4dffe7ccdd1b34f9ef1ffc06eccc3404676d4ec
murata <murata.naoya.infratop@gmail.com>
2021-05-08 13:15:59 +0900

    formクラスの生成・実装

--- src/main/java/jp/co/sss/lesson/controller/SessionController.java
+++ src/main/java/jp/co/sss/lesson/controller/SessionController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import jp.co.sss.lesson.form.LoginForm;
+
 @Controller
 public class SessionController {
 	@GetMapping("/login")
@@ -11,15 +13,21 @@ public class SessionController {
 		return "session/login";
 	}
 	
-	@GetMapping("/doLogin")
-	public String doLogin(String userId) {
-		System.out.println(userId);
-		return "session/login";
-	}
-	
 	@PostMapping("/postLogin")
 	public String postLogin(String submit) {
 		System.out.println(submit);
 		return "session/login";
+	}
+	
+	@GetMapping("/loginForm")
+	public String loginForm() {
+		return "session/loginForm";
+	}
+	
+	@PostMapping("/loginForm")
+	public String loginForm(LoginForm form) {
+		System.out.println(form.getUserId());
+		System.out.println(form.getPassword());
+		return "session/loginForm";
 	}
 }
---
+++ src/main/java/jp/co/sss/lesson/form/LoginForm.java
@@ -0,0 +1,21 @@
+package jp.co.sss.lesson.form;
+
+public class LoginForm {
+	private int userId;
+	private String password;
+	
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+}
